2fb3cb737cfc1f526ccdecf322eb1f4d1161d9e1
fjaschneider <33283311+fjaschneider@users.noreply.github.com>
2020-02-27 16:05:22 -0300

    Ready!

--- Join_tables.R
+++ Join_tables.R
@@ -1,10 +1,13 @@
 ## To Join tables
 
-directory <- 'data/data_30min/'
+directory <- 'data/data_30min/' ## Choose the directory
 list_tables <- list.files(directory)
 
 
+for (i in 1:length(list_tables)) {
+  table <- paste0(directory, "/", list_tables[i])
+  table <- read.csv(table)
+  
+  complet_table <- rbind(complet_table, table)
 
-
-
-rbind()
+}
