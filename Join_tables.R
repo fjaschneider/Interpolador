9c7e7db8ba0e3c200b61fbc381842f13f3744b56
fjaschneider <33283311+fjaschneider@users.noreply.github.com>
2020-02-27 16:54:57 -0300

    OK!!

--- Join_tables.R
+++ Join_tables.R
@@ -11,3 +11,9 @@ for (i in 1:length(list_tables)) {
   complet_table <- rbind(complet_table, table)
 
 }
+
+complet_table <- complet_table[ ,-1]
+complet_table <- complet_table[(order(as.Date(complet_table$data))), ]
+colnames(complet_table) <- c('Date_hour', 'P_mm_h', 'Q_l_s')
+
+write.csv(complet_table, file = 'data/df_BCT_30min.csv', row.names = FALSE)
